f01b03ff1a5edc3b84f9a1ec9ba501248c9e1f13
Naff Mwaura <naftali795@gmail.com>
2026-05-12 15:19:08 +0300

    Where everything happens in the the workspace

--- frontend/src/features/takeoff/ProjectTakeoffPage.tsx
+++ frontend/src/features/takeoff/ProjectTakeoffPage.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../auth/AuthContext";
 import { useOnlineStatus } from "../../hooks/useOnlineStatus";
